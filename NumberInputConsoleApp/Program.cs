807949dc3b1d596a95b9be2e7118f9c9da023e68
MaxSalut <m.bezverkhyi28@gmail.com>
2024-11-18 02:01:19 +0200

    Add project files.

---
+++ NumberInputConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿// File: Program.cs
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            while (true)
+            {
+            Console.WriteLine("Введіть ціле число:");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine($"Ви ввели число {number}");
+            }
+            else
+            {
+                Console.WriteLine("Будь ласка, введіть коректне ціле число.");
+            }
+            }
+        }
+    }
+}
